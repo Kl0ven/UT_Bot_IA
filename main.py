05e0922a4455f29e3016c9c157c0601a08abd0c6
Kloven <jloup.m@gmail.com>
2019-11-13 15:21:22 +0100

    time it function

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import gym
 import matplotlib.pylab as plt
 from utils import save, load
 import sys
-
+import time
+import numpy as np
+import datetime
 
 # For tensorboard to work you need to enable eager mode
 # But with eager mode enable the learning is slower due to some bug
 # https://github.com/tensorflow/tensorflow/issues/33052
-tf.compat.v1.disable_eager_execution()
+# tf.compat.v1.disable_eager_execution()
 
 MAX_EPSILON = 1
 MIN_EPSILON = 0.01
@@ -20,7 +22,8 @@ GAMMA = 0.99
 BATCH_SIZE = 50
 MEMORY_SIZE = 50000
 
-writer = tf.summary.create_file_writer("results")
+folder = datetime.datetime.now().strftime("%d-%m-%Y - %Hh %Mm %Ss")
+writer = tf.summary.create_file_writer("results/" + folder)
 
 env_name = 'MountainCar-v0'
 env = gym.make(env_name)
@@ -43,10 +46,13 @@ for i, arg in enumerate(sys.argv):
 num_episodes = 300
 cnt = 0
 action = ag._choose_action(env.reset())
+starttime = time.time() * 1000
+times = []
 with writer.as_default():
 	while cnt < num_episodes:
 		if cnt % 10 == 0:
 			print('Episode {} of {}'.format(cnt + 1, num_episodes))
+		times = []
 		while True:
 			next_state, reward, done, info = env.step(action)
 			action = ag.update(next_state, reward, done)
@@ -54,8 +60,12 @@ with writer.as_default():
 				ag.reset()
 				break
 			ag._replay()
+			currenttime = time.time() * 1000
+			times.append(currenttime - starttime)
+			starttime = currenttime
 		tf.summary.scalar("max_x", ag.max_x, step=cnt)
 		tf.summary.scalar("reward", ag.reward, step=cnt)
+		tf.summary.scalar("Time", np.mean(times), step=cnt)
 		writer.flush()
 		cnt += 1
 save(model)
